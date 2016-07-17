347363832f7607d956d88825a4db375874a69d6e
meteorlad <meteorlad@gmail.com>
2016-07-17 18:41:21 +0800

    Add VirtualBox Configure script

---
+++ scripts/virtualbox.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Installing the virtualbox guest additions
+VBOX_VERSION=$(cat /`whoami`/.vbox_version)
+cd /tmp
+mount -o loop /`whoami`/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /`whoami`/VBoxGuestAdditions_*.iso
